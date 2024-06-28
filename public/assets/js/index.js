63fdbd2251ca062357af4e1dfa88e48992ce4330
Amplify <not.amplify.x@gmail.com>
2024-06-28 11:07:40 -0400

    WHY DID IT DELETE

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,29 +1,7 @@
-"use strict";
-
-/**
- * @type {HTMLFormElement}
- */
 const form = document.getElementById("uv-form");
-/**
- * @type {HTMLInputElement}
- */
 const address = document.getElementById("uv-address");
-/**
- * @type {HTMLInputElement}
- */
-const searchEngine = document.getElementById("uv-search-engine");
-/**
- * @type {HTMLParagraphElement}
- */
-const error = document.getElementById("uv-error");
-/**
- * @type {HTMLPreElement}
- */
-const errorCode = document.getElementById("uv-error-code");
-
 const input = document.querySelector("input");
 
-// crypts class definition
 class crypts {
   static encode(str) {
     return encodeURIComponent(
@@ -48,11 +26,24 @@ class crypts {
   }
 }
 
+var wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
+
+async function setTransports() {
+  const transports = localStorage.getItem("transports") || "epoxy";
+  if (transports === "epoxy") {
+    await BareMux.SetTransport("EpxMod.EpoxyClient", { wisp: wispUrl });
+  } else if (transports === "libcurl") {
+    await BareMux.SetTransport("CurlMod.LibcurlClient", { wisp: wispUrl });
+  } else {
+    await BareMux.SetTransport("EpxMod.EpoxyClient", { wisp: wispUrl });
+  }
+}
+
 // Search function definition
 function search(input) {
   input = input.trim();  // Trim the input to remove any whitespace
   // Retrieve the search engine URL template from localStorage or use default
-  const searchTemplate = 'https://google.com/search?q=%s';
+  const searchTemplate = localStoage.getItem("search") || 'https://google.com/search?q=%s';
 
   try {
     // Try to treat the input as a URL
@@ -72,19 +63,22 @@ function search(input) {
   }
 }
 if ('serviceWorker' in navigator) {
-  var proxySetting = 'uv';
+  var proxySetting = localStorage.getItem("proxy") || 'uv';
   let swConfig = {
     'uv': { file: '/@/sw.js', config: __uv$config }
   };
 
   let { file: swFile, config: swConfigSettings } = swConfig[proxySetting];
 
+  navigator.serviceWorker.ready.then(async () => {
+    await setTransports()
+  })
   navigator.serviceWorker.register(swFile, { scope: swConfigSettings.prefix })
     .then((registration) => {
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
       form.addEventListener('submit', async (event) => {
         event.preventDefault();
-        
+
         let encodedUrl = swConfigSettings.prefix + crypts.encode(search(address.value));
         location.href = encodedUrl;
       });
